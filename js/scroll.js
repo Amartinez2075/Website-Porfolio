b1d7276f03a0ecbfe4ef13d0d114b5856a84e0d5
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-06-16 13:03:57 -0500

    Added scroll functionality

---
+++ js/scroll.js
@@ -0,0 +1,14 @@
+
+  // Smooth scroll functionality
+  document.querySelectorAll('.Menu-Bar a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+
+
+
